f261a02e4d94c58b4fb2797bd125b2f56de36fd0
GOOTH, Yutaka <ygotoh@ygotoh.jp>
2015-03-02 16:01:56 +0900

    Convert specs to RSpec 3.2.1 syntax with Transpec
    This conversion is done by Transpec 3.1.0 with the following command:
        transpec spec
    
    * 76 conversions
        from: obj.should
          to: expect(obj).to
    
    * 42 conversions
        from: == expected
          to: eq(expected)
    
    * 17 conversions
        from: be_true
          to: be_truthy
    
    * 17 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    * 4 conversions
        from: lambda { }.should
          to: expect { }.to
    
    * 4 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 2 conversions
        from: be_false
          to: be_falsey
    
    * 2 conversions
        from: obj.should_not_receive(:message)
          to: expect(obj).not_to receive(:message)
    
    * 1 conversion
        from: collection.should have(n).items
          to: expect(collection.size).to eq(n)
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/rrd_spec.rb
+++ spec/rrd_spec.rb
@@ -12,8 +12,8 @@ describe RRD do
       line RRD_FILE, :memory => :average, :color => "#0000FF", :label => "Memory"
     end
     
-    result.should be_true
-    File.should be_file(IMG_FILE)
+    expect(result).to be_truthy
+    expect(File).to be_file(IMG_FILE)
   end
   
   it "should create a graph using graph!" do
@@ -22,12 +22,12 @@ describe RRD do
       line RRD_FILE, :memory => :average, :color => "#0000FF", :label => "Memory"
     end
     
-    result.should be_true
-    File.should be_file(IMG_FILE)
+    expect(result).to be_truthy
+    expect(File).to be_file(IMG_FILE)
   end  
 
   it "should list all bang methods" do
-    (RRD.methods & RRD::BANG_METHODS).should == RRD::BANG_METHODS
+    expect(RRD.methods & RRD::BANG_METHODS).to eq(RRD::BANG_METHODS)
   end
   
   it "should create a graph using advanced DSL" do
@@ -42,8 +42,8 @@ describe RRD do
       print_value "mem_avg", :format => "%6.2lf %SB"
     end
     
-    result.should be_true
-    File.should be_file(IMG_FILE)
+    expect(result).to be_truthy
+    expect(File).to be_file(IMG_FILE)
   end
 
 
@@ -65,7 +65,7 @@ describe RRD do
       [1266944790, 536870912.0, 0.0022, 268435456.0],
     ]
 
-    data[0,4].should == expected_data    
+    expect(data[0,4]).to eq(expected_data)    
   end  
 
   it "should export data using xport!" do
@@ -86,6 +86,6 @@ describe RRD do
       [1266944790, 536870912.0, 0.0022, 268435456.0],
     ]
 
-    data[0,4].should == expected_data    
+    expect(data[0,4]).to eq(expected_data)    
   end  
 end
